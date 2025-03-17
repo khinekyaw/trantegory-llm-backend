0b18e79a31e18fea2405a5a6be9107ca5b7c5761
daniel <danielrio126@gmail.com>
2025-03-17 15:04:56 +0700

    feat: add swagger

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
 const transactionRoutes = require('./routes/transactions');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Load Swagger document
+const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
+
 app.use(cors());
 app.use(express.json());
 
+// Serve Swagger documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// API routes
 app.use('/api/transactions', transactionRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+  console.log(`API Documentation available at http://localhost:${port}/api-docs`);
 }); 
